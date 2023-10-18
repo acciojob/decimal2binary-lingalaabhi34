6a011b70008c43618a81ef5931ed4f287d48add5
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-18 13:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
+	let out = "";
+	while(num>=2){
+		remainder = num%2;
+		out = remainder + out ;
+		num = parseInt(num/2) ;
+	}
+	if(num ===1){
+		out = 1 + out ;
+	}
   
 }
 
